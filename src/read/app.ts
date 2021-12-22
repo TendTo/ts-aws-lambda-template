9c46c75fbe048b3adbe97b86d98792f34c7d042e
Ernesto Casablanca <casablancaernesto@gmail.com>
2021-12-22 16:38:35 +0100

    add: read all endpoint

--- src/read/app.ts
+++ src/read/app.ts
@@ -1,6 +1,6 @@
 import { LambdaHandler } from './types/lambda';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, GetCommand, QueryCommand } from "@aws-sdk/lib-dynamodb";
 
 export const handler: LambdaHandler = async (event) => {
     const client = new DynamoDBClient({ region: process.env.REGION ?? 'eu-west-1' });
@@ -8,20 +8,44 @@ export const handler: LambdaHandler = async (event) => {
         marshallOptions: { convertClassInstanceToMap: true },
         unmarshallOptions: { wrapNumbers: true }
     });
-    const getCommand = new GetCommand({
+
+    // Only a single item is returned
+    if (event.pathParameters?.id) {
+        const getCommand = new GetCommand({
+            TableName: process.env.TABLE_NAME ?? '',
+            Key: {
+                pk: event.pathParameters?.id,
+                sk: "article",
+            }
+        });
+        const res = await dbDocument.send(getCommand);
+        return {
+            "statusCode": 200,
+            "body": JSON.stringify({
+                ok: true,
+                result: res.Item,
+            })
+        };
+    }
+
+    // All articles are returned
+    const queryCommand = new QueryCommand({
         TableName: process.env.TABLE_NAME ?? '',
-        Key: {
-            pk: event.pathParameters.id,
-            sk: "article",
+        IndexName: process.env.INDEX_NAME ?? 'GSI_1',
+        KeyConditionExpression: '#sk = :sk',
+        ExpressionAttributeNames: {
+            '#sk': 'sk'
+        },
+        ExpressionAttributeValues: {
+            ':sk': 'article'
         }
     });
-    const res = await dbDocument.send(getCommand);
-
+    const res = await dbDocument.send(queryCommand);
     return {
         "statusCode": 200,
         "body": JSON.stringify({
             ok: true,
-            result: res.Item,
+            result: res.Items,
         })
     };
 };
